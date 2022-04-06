209aa7736d224e3dbba7432acc38033304e09a7d
Jetro2203 <102870049+Jetro2203@users.noreply.github.com>
2022-04-06 15:14:42 +0200

    Update Control.lua

--- System/Control.lua
+++ System/Control.lua
@@ -1,9 +1,9 @@
 --[[
 
-    Name = Installer.lua
-    Version = 2.0
-    Date = 2/4/2022
-    Time = 18:41
+    Name = Control.lua
+    Version = 1.2.4
+    Date = 6/4/2022
+    Time = 14:56
     Author = Jetro
 
 ]]
@@ -11,145 +11,360 @@
 -- Variables
 
 local name = "ReactorControl"
-local version = "2.0"
+local filename = name.."/System/Control.lua"
+local version = "1.2.4"
 
-local logFile = "OS/logs/Installer.log"
-
-local files = {
-    ["startup.lua"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/startup.lua",
-    [name.."/System/Control.lua"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/System/Control.lua",
-    [name.."/System/Display.lua"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/System/Display.lua",
-    [name.."/System/Redstone.lua"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/System/Redstone.lua",
-    [name.."/System/Config.cfg"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/System/Config.cfg",
-    [name.."/Symbols/Stop.nfp"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/Symbols/Stop.nfp",
-    [name.."/Symbols/Start.nfp"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/Symbols/Start.nfp",
-    [name.."/Symbols/RadioActive.nfp"] = "https://raw.githubusercontent.com/Jetro2203/ReactorControl/main/Symbols/Radioactive.nfp",
-    ["OS/APIs/screen.api"] = "https://raw.githubusercontent.com/Jetro2203/APIs/main/screen.api",
+local file = {
+    config = "ReactorControl/System/Config.cfg",
+    log = "ReactorControl/System/log.log"
 }
+
+local reactor = {}
+local turbine = {}
+local battery = {}
+local mon = {}
+
+local last = {
+    page = {
+        term,
+        mon,
+    },
+    button = {
+        automode,
+    }
+}
+
+local page = {
+    warning = 1,
+    error = 1,
+}
+
+local tW, tH
 
 -- Functions
 
-function log(data)
-    myLog = fs.open(logFile,"a")
-    print(data)
-    myLog.write(data.."\n")
+function config_read()
+    if fs.exists(file.config) then
+        myConfig = fs.open(file.config,"r")
+        config = textutils.unserialise(myConfig.readAll())
+        myConfig.close()
+    else
+        log("error","unable to find configfile")
+        error()
+    end
+end
+
+function config_write()
+    if config then
+        table.sort(config)
+        myConfig = fs.open(file.config,"w")
+        myConfig.write(textutils.serialise(config))
+        myConfig.close()
+    else
+        log("error","Config is nill")
+        error()
+    end
+end
+
+function log(logType, data)
+    myLog = fs.open(file.log,"a")
+    myLog.write("["..string.upper(logType).."] ["..filename.."] "..data.."\n")
     myLog.close()
-end
-
-function delete_file(location)
-    log("Deleting files: "..location)
-    fs.delete(location)
-end
-
-function delete_all(table_files)
-    log("\nDeleting all files...")
-    for location, URL in pairs(table_files) do
-        delete_file(location)
+    if string.lower(logType) == "error" then
+        table.insert(config.error,"["..logType.."] ["..filename.."] "..data)
+    elseif string.lower(logType) == "warning" then
+        table.insert(config.warning,"["..logType.."] ["..filename.."] "..data)
     end
-    log("\nComplete")
+    config_write()
 end
 
-function write_file(location, data)
-    if fs.exists(location) then
-        log("Unable to create new file: file already exists: "..location)
-        error("Unable to create new file: file already exists: "..location)
-    else
-        myFile = fs.open(location, "w")
-        myFile.write(data)
-        myFile.close()
+function init_peripheral()
+    PList = peripheral.getNames()
+    for i = 1, #PList do
+        if peripheral.getType(PList[i]) == "BigReactors-Reactor" then
+            table.insert(reactor,PList[i])
+        elseif peripheral.getType(PList[i]) == "BigReactors-Turbine" then
+            table.insert(turbine,PList[i])
+        elseif peripheral.getType(PList[i]) == "capacitor_bank" then
+            table.insert(battery,PList[i])
+        elseif peripheral.getType(PList[i]) == "monitor" then
+            table.insert(mon,PList[i])
+        end
+        table.sort(reactor)
+        table.sort(turbine)
+        table.sort(battery)
+        table.sort(mon)
+    end
+
+    for i = 1, #reactor do
+        reactor[i] = peripheral.wrap(reactor[i])
+    end
+    for i = 1, #turbine do
+        turbine[i] = peripheral.wrap(turbine[i])
+    end
+    for i = 1, #battery do
+        battery[i] = peripheral.wrap(battery[i])
+    end
+    for i = 1, #mon do
+        mon[i] = peripheral.wrap(mon[i])
     end
 end
 
-function download_file(URL,location)
-    log("Downloading "..URL.." as "..location)
-    if http.checkURL(URL) then
-        myGithub = http.get(URL)
-        data = myGithub.readAll()
-        myGithub.close()
-        write_file(location,data)
-    else
-        error("Unable to find URL:"..URL)
+function init_term()
+    tW,tH = term.getSize()
+    draw_menu_t()
+end
+
+function draw_menu_t()
+    screen.clear(colors.blue,colors.black)
+    screen.drawText(1,1,name.." v"..version)
+    screen.drawText(tW-string.len(config.page.term),1,string.upper(config.page.term))
+    if config.page.term == "home" then
+        screen.drawText(1,3,"STATE:")
+        screen.drawText(11,3,config.program_state,colors.blue,(string.lower(config.program_state) == "unknown" and colors.red) or colors.black)
+        screen.drawText(1,5,"WARNINGS:",colors.blue,colors.black)
+        screen.drawText(11,5,#config.warning,colors.blue,(#config.warning == 0 and colors.lime) or colors.red)
+        screen.drawText(1,6,"ERRORS:",colors.blue,colors.black)
+        screen.drawText(11,6,#config.error,colors.blue,(#config.error == 0 and colors.lime) or colors.red)
+        screen.drawText(1,8,"CONTROLS",colors.blue,colors.black)
+        screen.drawText(1,9,"S - edit settings")
+        screen.drawText(1,10,"W - view warnings")
+        screen.drawText(1,11,"E - view errors")
+        screen.drawText(1,12,"C - commands")
+        screen.drawText(1,13,"U - update")
+    elseif config.page.term == "settings" then
+        
+    elseif config.page.term == "warnings" then
+        screen.drawText(1,3,"WARNINGS:",colors.blue,colors.black)
+        screen.drawText(11,3,#config.warning,colors.blue,(#config.warning == 0 and colors.lime) or colors.red)
+        screen.drawRect(2,5,tW-2,tH-5,colors.gray,true,colors.gray)
+        screen.drawText(2,5,"Page: ("..page.warning.."/"..math.ceil(((#config.warning == 0 and 1) or #config.warning)/12)..")",colors.gray,colors.black)
+        screen.drawText(tW-17,5,"[ PREV ] [ NEXT ]")
+        if #config.warning == 0 then
+            screen.drawText(2,6,"No warnings")
+        else
+            for i = 1, #config.warning do
+                if i > (page.warning-1)*12 and i <= (page.warning-1)*12+12 then
+                    screen.drawText(2,5+(i-(page.warning-1)*12),config.warning[i])
+                    screen.drawText(tW-3,5+(i-(page.warning-1)*12),"[X]")
+                end
+            end
+        end
+    elseif config.page.term == "errors" then
+        screen.drawText(1,3,"ERRORS:",colors.blue,colors.black)
+        screen.drawText(11,3,#config.error,colors.blue,(#config.error == 0 and colors.lime) or colors.red)
+        screen.drawRect(2,5,tW-2,tH-5,colors.gray,true,colors.gray)
+        screen.drawText(2,5,"Page: ("..page.error.."/"..math.ceil(((#config.error == 0 and 1) or #config.error)/12)..")",colors.gray,colors.black)
+        screen.drawText(tW-17,5,"[ PREV ] [ NEXT ]")
+        if #config.error == 0 then
+            screen.drawText(2,6,"No errors")
+        else
+            for i = 1, #config.error do
+                if i > (page.error-1)*12 and i <= (page.error-1)*12+12 then
+                    screen.drawText(2,5+(i-(page.error-1)*12),config.error[i])
+                    screen.drawText(tW-3,5+(i-(page.error-1)*12),"[X]")
+                end
+            end
+        end
+    elseif config.page.term == "commands" then
+
+    elseif config.page.term == "update" then
+        screen.drawText(1,3,"This will run the reboot the computer in Installer mode",colors.blue,colors.black)
+        screen.drawText(1,4,"Are you sure [Y/N]\n")
+        event, key = os.pullEvent("key")
+        if key == keys.y then
+            sleep(.1)
+            myStartup = fs.open("OS/files/StartupMode.txt","w")
+            myStartup.write(name.."_Installer")
+            myStartup.close()
+            os.reboot()
+        elseif key == keys.n then
+            screen.drawText(1,6,"update cancelled")
+            sleep(1)
+        end
+        config.page.term = "home"
+        config_write()
     end
 end
 
-function download_all(table_files)
-    log("\nDownloading all files...")
-    for location, URL in pairs(table_files) do
-        download_file(URL,location)
+function reactor_start()
+    for i = 1, #reactor do
+        if config.reactor.lockout[i] then
+            reactor[i].setActive(true)
+        end
     end
-    log("\nComplete")
 end
 
-function update()
-    delete_all(files)
-    download_all(files)
-    log("\nLogFile: "..logFile)
-    log("Rebooting...")
-    myStartup = fs.open("OS/files/StartupMode.txt","w")
-    myStartup.write(name.."_normal")
-    sleep(2)
-    os.reboot()
+function reactor_stop()
+    for i = 1, #reactor do
+        reactor[i].setActive(false)
+    end
+end
+
+function turbine_start()
+    for i = 1, #turbine do
+        if config.turbine.lockout[i] then
+            turbine[i].setActive(true)
+        end
+    end
+end
+
+function turbine_stop()
+    for i = 1, #turbine do
+        turbine[i].setActive(false)
+        turbine[i].setInductorEngaged(false)
+    end
+end
+
+function control_touch()
+    event, a, b, c = os.pullEvent()
+    config_read()
+    if event == "monitor_touch" then
+        side = a
+        x = b
+        y = c
+        if x >= 1 and x <= 9 and y == 1 then
+            config.page.mon = "home"
+        elseif x >= 10 and x <= 19 and y == 1 then
+            config.page.mon = "reactor"
+        elseif x >= 20 and x <= 29 and y == 1 then
+            config.page.mon = "turbine"
+        elseif x >= 30 and x <= 39 and y == 1 then
+            config.page.mon = "battery"
+        end
+        
+        if last.page.mon ~= config.page.mon then
+            config_write()
+        end
+        last.page.mon = config.page.mon
+
+        if config.page.mon == "home" then
+            if x >= 12 and x <= 15 and y == 3 then
+                if config.button.automode then
+                    reactor_stop()
+                    turbine_stop()
+                    config.button.automode = false
+                elseif not(config.button.automode) then
+                    config.button.automode = true
+                end
+            elseif x >= 95 and x <= 119 and y >= 4 and y <= 12 then
+                reactor_start()
+                turbine_start()
+                config.button.automode = true
+            elseif x >= 95 and x <= 119 and y >= 14 and y <= 22 then
+                reactor_stop()
+                turbine_stop()
+                config.button.automode = false
+            end
+
+            if last.button.automode ~= config.button.automode then
+                config_write()
+            end
+            last.button.automode = config.button.automode
+
+        elseif config.page.mon == "reactor" then
+        
+        elseif config.page.mon == "turbine" then
+        
+        elseif config.page.mon == "battery" then
+
+        end
+    elseif event == "key" then
+        key = a
+        if key == keys.h then
+            if config.page.term ~= "home" then
+                config.page.term = "home"
+            end
+        elseif key == keys.s then
+            if config.page.term == "settings" then
+                config.page.term = "home"
+            else
+                config.page.term = "settings"
+            end
+        elseif key == keys.w then
+            if config.page.term == "warnings" then
+                config.page.term = "home"
+            else
+                config.page.term = "warnings"
+            end
+        elseif key == keys.e then
+            if config.page.term == "errors" then
+                config.page.term = "home"
+            else
+                config.page.term = "errors"
+            end
+        elseif key == keys.c then
+            if config.page.term == "commands" then
+                config.page.term = "home"
+            else
+                config.page.term = "commands"
+            end
+        elseif key == keys.u then
+            sleep(.1)
+            if config.page.term == "update" then
+                config.page.term = "home"
+            else
+                config.page.term = "update"
+            end
+        end
+        if last.page.term ~= config.page.term then
+            config_write()
+        end
+        last.page.term = config.page.term
+    elseif event == "mouse_click" then
+        side = a
+        x = b
+        y = c
+        if config.page.term == "warnings" then
+            if x >= 34 and x <= 41 and y == 5 then
+                if page.warning > 1 then
+                    page.warning = page.warning - 1
+                end
+            elseif x >= 43 and x <= 50 and y == 5 then
+                if page.warning < math.ceil(((#config.warning == 0 and 1) or #config.warning)/12) then
+                    page.warning = page.warning + 1
+                end
+            end
+            for i = 1, #config.warning do
+                if x >= 48 and x <= 50 and y == 5+(i-(page.warning-1)*12) then
+                    table.remove(config.warning,i)
+                    config_write()
+                end
+            end
+        elseif config.page.term == "errors" then
+            if x >= 34 and x <= 41 and y == 5 then
+                if page.error > 1 then
+                    page.error = page.error - 1
+                end
+            elseif x >= 43 and x <= 50 and y == 5 then
+                if page.error < math.ceil(((#config.error == 0 and 1) or #config.error)/12) then
+                    page.error = page.error + 1
+                end
+            end
+            for i = 1, #config.error do
+                if x >= 48 and x <= 50 and y == 5+(i-(page.error-1)*12) then
+                    table.remove(config.error,i)
+                    config_write()
+                end
+            end
+        end
+    end
+end
+
+function start()
+    config_read()
+    init_peripheral()
+    init_term()
+    main()
+end
+
+function main()
+    while true do
+        draw_menu_t()
+        control_touch()
+    end
 end
 
 -- Main
- 
-shell.run("clear")
- 
-if fs.exists(logFile) then
-    fs.delete(logFile)
-end
-myLog = fs.open(logFile,"w")
-myLog.close()
- 
-log(name.." Installer\nVersion "..version.."\n")
- 
-log("[1] Update Installer")
-log("[2] Install "..name)
-input = read()
-x,y = term.getCursorPos()
-term.setCursorPos(x,y-1)
-log(input)
-if input == "1" then
-    log("\nDeleting old installer...")
-    fs.delete(name.."/Installer.lua")
-    log("\nComplete")
-    log("\nDownloading new installer")
-    shell.run("wget","https://github.com/Jetro2203/ReactorControl/blob/main/Installer.lua",name.."/Installer.lua")
-    log("\nComplete")
-    log("started new installer...")
-    sleep(1)
-    shell.run(name.."/Installer.lua")
-elseif input == "2" then
-    if fs.exists(name.."/") then
-        log("\nold version found")
-        log("do you want to update? [Y/N]")
-        input = string.upper(read())
-        x,y = term.getCursorPos()
-        term.setCursorPos(x,y-1)
-        log(input)
-        if input == "Y" then
-            update()
-        elseif input == "N" then
-            log("")
-            log("Cancelling update")
-            sleep(1)
-            shell.run("clear")
-        end
-    else
-        log("\nno old version found")
-        log("do you want to install "..name.."? [Y/N]")
-        input = string.upper(read())
-        x,y = term.getCursorPos()
-        term.setCursorPos(x,y-1)
-        log(input)
-        if input == "Y" then
-            update()
-        elseif input == "N" then
-            log("")
-            log("Cancelling update")
-            sleep(1)
-            shell.run("clear")
-        end
-    end
-end
- 
+
+start()
